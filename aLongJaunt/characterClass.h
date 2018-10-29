df9214249a69df545a44925bee4be04fca99e158
Transpennine <44532368+Transpennine@users.noreply.github.com>
2018-10-29 18:57:53 -0400

    First Commit - main and player class beginning

---
+++ aLongJaunt/characterClass.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class characterClass {
+private:
+	string className;
+	int classNum;
+	int health;
+	int charm;
+	int strength;
+	int magic;
+	int speed;
+public:
+
+};
---
+++ aLongJaunt/main.cpp
@@ -0,0 +1,16 @@
+//Program name: aLongJaunt.cpp
+//Author: Anastasia Martindale
+//Date last updated: 10/18/2018
+//Purpose: word-based fantasy-adventure game with elements of RPG 
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
