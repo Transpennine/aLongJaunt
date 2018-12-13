15d9aadd6f76f77c2e4e3f62ec0fea9990d696a8
Transpennine <44532368+Transpennine@users.noreply.github.com>
2018-12-12 21:15:44 -0500

    Create Messages.h

---
+++ aLongJaunt/Messages.h
@@ -0,0 +1,15 @@
+#include <string>	
+#include <iostream>
+using namespace std;
+
+extern string charmMessageCheck();
+extern string strengthMessageCheck();
+extern string magicMessageCheck();
+extern string speedMessageCheck();
+
+extern string bypassSuccess();
+extern string bypassFail();
+
+const string warriorSelection();
+//extern string mageChoice();
+//extern string spyChoice();
